fbf669ed6eb296efd45a57ff382405b799e9de90
cybilod <rangelmaciel21@gmail.com>
2024-09-30 00:07:07 -0500

    Primera version

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+
+          <img src="https://dreamorganics.com/wp-content/uploads/2016/04/gradient-dream-organics-logo-1000-x-1000px-instagram.jpg" className="logo" alt="Vite logo" />
+      </div>
+      <h1>¡BIENVENIDO!</h1>
+      <div className="card">
+        
+        <p>
+          ¿Que es mas acido que un limon?....Exacto dos limones :)
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
